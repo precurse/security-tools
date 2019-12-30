b9c3fe9d9517f2d135bad6da4cfe417f34b44597
Andrew Klaus <andrew@aklaus.ca>
2019-12-30 13:48:13 -0700

    Moving travis tests to test.sh script

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+IMAGE_NAME=precurse/security-tools
+
+function docker_test {
+    docker run -it $IMAGE_NAME $@
+}
+
+docker_test nmap --version
+docker_test wpscan --version
+docker_test gobuster -h
+docker_test binwalk /bin/date
+docker_test john
+docker_test cewl --help
+docker_test ffuf -V
+docker_test ncrack --version
+docker_test r2 -version
+docker_test bettercap -version
+docker_test responder.py --version
+docker_test dnschef.py --help
