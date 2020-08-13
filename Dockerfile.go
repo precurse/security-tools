cfcfa52b6605170ab36a0b033672bc2e2b930a45
Andrew Klaus <andrew@aklaus.ca>
2020-08-13 10:31:35 -0600

    Adding Google oauth2 and sheets API

--- Dockerfile.go
+++ Dockerfile.go
@@ -5,5 +5,7 @@ ENV CGO_ENABLED 1
 WORKDIR /work
 
 RUN go get golang.org/x/net/html \
+	&& go get -u google.golang.org/api/sheets/v4 \
+	&& go get -u golang.org/x/oauth2/google \
 	&& go get -u github.com/gocolly/colly/... \
 	&& go get github.com/mattn/go-sqlite3
